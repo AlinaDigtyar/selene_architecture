00a2996a6b943741c2d72f12b4718033ca311ad7
Alina Digtyar <alinadigtyar@gmail.com>
2017-01-14 16:21:55 +0200

    initial commit

---
+++ src/scripts/main.js
@@ -0,0 +1,14 @@
+;(function($){
+	"use strict";
+
+$(function(){
+		$('.ba-slider').slick({
+			dots: false,
+			arrows: false,
+			infinite: true,
+			slidesToShow: 4,
+			slidesToScroll: 1
+		});
+	})
+	
+})(jQuery);
